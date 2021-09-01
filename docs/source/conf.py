2202523b6a4aa69907516a813329f6fcd30403c8
Roman Simakov <roman.simakov@gmail.com>
2021-09-01 21:45:07 +0300

    Cleanup the template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RedDatabaseLab'
+copyright = '2021, Roman Simakov'
+author = 'Роман Симаков'
 
 release = '0.1'
 version = '0.1.0'
